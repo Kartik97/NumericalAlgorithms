bd891bf45b00a13622233a709466ef03f3efb3cd
kartik97 <mcs192563@iitd.ac.in>
2020-02-24 05:37:04 +0530

    Upper

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -28,31 +28,26 @@ def gridLaplacian(m1, m2):
     return L
 
 def incompleteCholesky(A):
-    m = A.shape[0]
-    A 
+    m = A.shape[1]
     R = sparse.dok_matrix((m,m))
     for i in range(m):
         R[i,i] = math.sqrt(A[i,i])
-        R[i+1:,i] = A[i+1:,i]/R[i,i]
-        b = A[i+1:,i]
-        b = (b@b.conj().T)/A[i,i]
-        # print(b,b.shape,"khatam")
+        R[i,i+1:] = A[i,i+1:]/R[i,i]
+        # R[i+1:,i] = A[i+1:,i]/R[i,i]
+        b = A[i,i+1:]
+        # b = A[i+1:,i]
+        b = (b.conj().T@b)/A[i,i]
+        # b = (b@b.conj().T)/A[i,i]
         for j in range(m-i-1):
-            # print(i)
             (x,y) = A[i+1:,i+1:].getrow(j).nonzero()
-            # print(b)
-            # print(x,y,j)
             for k in range(len(x)):
                 A[i+1+j,y[k]+i+1] = A[j+i+1,y[k]+i+1]-b[j,y[k]]
-        # print(A,"A khatam")
-    print(R)
-    # print(A)
+    return R
 
 
 m1 = 2
 m2 = 2
 L = gridLaplacian(m1,m2)
 A = L + 20*sparse.eye(m1*m2)/(m1**2 + m2**2)
-# test = sparse.dok_matrix([[3,0,-1,-1,0,-1],[0,2,0,-1,0,0],[-1,0,3,0,-1,0],[-1,-1,0,2,0,-1],[0,0,-1,0,3,-1],[-1,0,0,-1,-1,4]],dtype=np.float32)
-incompleteCholesky(test)
-# print(A)
+test = sparse.dok_matrix([[3,0,-1,-1,0,-1],[0,2,0,-1,0,0],[-1,0,3,0,-1,0],[-1,-1,0,2,0,-1],[0,0,-1,0,3,-1],[-1,0,0,-1,-1,4]],dtype=np.float32)
+R = incompleteCholesky(test)
