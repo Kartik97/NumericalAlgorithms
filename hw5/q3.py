6f8921037c4e4ff8775c2e291b8d350ed3707ece
Kartik Jain <jain.kartik80@yahoo.com>
2020-08-15 16:20:36 +0530

    added newton's basis

--- hw5/q3.py
+++ hw5/q3.py
@@ -3,17 +3,21 @@ from scipy import linalg
 
 def createMatrix(t0,t1,y0,y1,m0,m1):
     A=np.zeros((4,4))
+    # A[0,0]=A[1,0]=A[2,1]=A[3,1]=1
+    # A[0,1]=t0
+    # A[0,2]=t0**2
+    # A[0,3]=t0**3
+    # A[1,1]=t1
+    # A[1,2]=t1**2
+    # A[1,3]=t1**3
+    # A[2,2]=2*t0
+    # A[2,3]=3*t0*t0
+    # A[3,2]=2*t0 
+    # A[3,3]=3*t1*t1
     A[0,0]=A[1,0]=A[2,1]=A[3,1]=1
-    A[0,1]=t0
-    A[0,2]=t0**2
-    A[0,3]=t0**3
-    A[1,1]=t1
-    A[1,2]=t1**2
-    A[1,3]=t1**3
-    A[2,2]=2*t0
-    A[2,3]=3*t0*t0
-    A[3,2]=2*t0 
-    A[3,3]=3*t1*t1
+    A[1,1]=A[3,2]=t1-t0
+    A[2,2]=t0-t1
+    A[3,3]=(t1-t0)**2
 
     b = np.array([y0,y1,m0,m1])
     return A,b
@@ -21,10 +25,11 @@ def createMatrix(t0,t1,y0,y1,m0,m1):
 def search(t,tnew):
     # Using Linear Search
     t=t.T[0]
+    n=t.shape[0]
     for i in range(0,t.shape[0]-1):
         if(t[i]<=tnew and t[i+1]>tnew):
-            return i
-    return t.shape[0]-2
+            return i,t[i],t[i+1]
+    return n-2,t[n-2],t[n-1]
     # Using Binary Search
     # l,r=0,t.shape[0]-1
     # while l<=r:
@@ -41,8 +46,23 @@ def search(t,tnew):
     #         l=m+1
     # return l
 
+# def piecewiseCubic(t,y,m,tnew):
+#     # Using Monomial Basis
+#     t=np.reshape(t,(-1,1))
+#     y=np.reshape(y,(-1,1))
+#     m=np.reshape(m,(-1,1))
+#     n=t.shape[0]
+#     coeffs=[]
+#     for i in range(0,n-1):
+#         A,b = createMatrix(t[i],t[i+1],y[i],y[i+1],m[i],m[i+1])
+#         a = linalg.solve(A,b)
+#         coeffs.append(a)
+#     i=search(t,tnew)
+#     factor=np.array([[1.0],[tnew],[tnew**2],[tnew**3]])
+#     return coeffs[i].T@factor
+
 def piecewiseCubic(t,y,m,tnew):
-    # Using Monomial Basis
+#     # Using Newton's Basis
     t=np.reshape(t,(-1,1))
     y=np.reshape(y,(-1,1))
     m=np.reshape(m,(-1,1))
@@ -50,10 +70,10 @@ def piecewiseCubic(t,y,m,tnew):
     coeffs=[]
     for i in range(0,n-1):
         A,b = createMatrix(t[i],t[i+1],y[i],y[i+1],m[i],m[i+1])
-        a = linalg.solve(A,b)
+        a = linalg.solve_triangular(A,b,lower=True)
         coeffs.append(a)
-    i=search(t,tnew)
-    factor=np.array([[1.0],[tnew],[tnew**2],[tnew**3]])
+    i,t0,t1=search(t,tnew)
+    factor=np.array([[1.0],[tnew-t0],[(tnew-t0)*(tnew-t1)],[(tnew-t0)*(tnew-t0)*(tnew-t1)]])
     return coeffs[i].T@factor
 
 def denominator(t0,t1,t2):
@@ -71,7 +91,7 @@ def estimateDerivatives(t,y):
     derivatives.append(term1+term2+term3)
     for i in range(1,n-1):
         term1=y[i-1]*(t[i]-t[i+1])/denominator(t[i-1],t[i],t[i+1])
-        term2=y[i]*(2*t[i]-t[i-1]-t[i])/denominator(t[i],t[i-1],t[i+1])
+        term2=y[i]*(2*t[i]-t[i-1]-t[i+1])/denominator(t[i],t[i-1],t[i+1])
         term3=y[i+1]*(t[i]-t[i-1])/denominator(t[i+1],t[i-1],t[i])
         derivatives.append(term1+term2+term3)
     term1=y[n-3]*(t[n-1]-t[n-2])/denominator(t[n-3],t[n-2],t[n-1])
@@ -80,5 +100,7 @@ def estimateDerivatives(t,y):
     derivatives.append(term1+term2+term3)
     return derivatives
 
-print(piecewiseCubic([1.3,1.6,1.9],[0.620086,0.4554022,0.2818186],[-0.5220232,-0.5648959,-0.5811571],1.5))  #0.5118277-0.512165
+print(piecewiseCubic([1.3,1.6,1.9],[0.620086,0.4554022,0.2818186],[-0.5220232,-0.5648959,-0.5811571],1.9))  #0.5118277-0.512165
 print(estimateDerivatives([1.3,1.6,1.9],[0.620086,0.4554022,0.2818186]))
+
+
