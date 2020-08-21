f81f3921529ddd00df0f1e31632067b749f93cff
kartik97 <jain.kartik80@yahoo.com>
2020-08-22 01:13:11 +0530

    line search

--- hw6/q3.py
+++ hw6/q3.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-def calculateGradient(X,var,links,m,n,p):
+def calculateGradient(X,var,links,k,m,n,p):
     # X = (m*k), links=[[i,j]....[]]
-    k=X.shape[1]
     grad = np.zeros((n,k))
     for i in links:
         xi=var[i[0]-m-1] if i[0]>m else X[i[0]-1]
@@ -19,33 +18,34 @@ def f(X,var,links):
     for i in links:
         xi=var[i[0]-m-1] if i[0]>m else X[i[0]-1]
         xj=var[i[1]-m-1] if i[1]>m else X[i[1]-1]
-        # print(xi,xj,xi-xj)
         cost+=np.linalg.norm(xi-xj,ord=2)**p
     return cost
 
-def lineSearch(X,var,links,grad):
+def lineSearch(X,var,links,deltax,k,m,n,p):
     t=1
-    alpha,beta=0.01,0.9
-    deltax = -grad
-    print(f(X,var+t*deltax,links),f(X,var,links)+alpha*t*(grad@deltax.T))
-    # while f(X,var+):
-    #     t=beta*t
-    # return t
+    alpha,beta=0.4,0.8
+    grad=calculateGradient(X,var,links,k,m,n,p).flatten()
+    while f(X,var+t*deltax,links)>f(X,var,links)+alpha*t*grad@deltax.flatten().T:
+        t=beta*t
+    return t
 
-def gradientDescent(X,links,m,n,p,eplison=1e-4):
-    gradnorm = 1
+def gradientDescent(X,links,k,m,n,p,eplison=1e-5):
     k=X.shape[1]
+    # X=X.flatten()
+    gradnorm = 1
     var = np.zeros((n,k)) # Initialising all n points at zero
-    # while gradnorm>eplison:
-    grad=calculateGradient(X,var,links,m,n,p)
-    t=lineSearch(X,var,links,grad)
-        # var=var+t*grad
-        # gradnorm=norm(grad)
+    while gradnorm>eplison:
+        d=-calculateGradient(X,var,links,k,m,n,p)
+        t=lineSearch(X,var,links,d,k,m,n,p)
+        var=var+t*d
+        gradnorm=np.linalg.norm(calculateGradient(X,var,links,k,m,n,p).flatten(),ord=2)
+        print(gradnorm)
     return var
 
 X=np.array([[1,1],[2,1],[2,2],[1,2]])
-# links=np.array([[5,3],[5,6],[5,2],[5,1],[6,4],[6,1],[6,2]])
-links=np.array([[5,2]])
-m,n=4,2
+links=np.array([[5,3],[5,6],[5,2],[5,1],[6,4],[6,1],[6,2]])
+# links=np.array([[5,2]])
+k,m,n=2,4,2
 p=2
-gradientDescent(X,links,m,n,p)
+
+print(gradientDescent(X,links,k,m,n,p))
